e35c4f26723f8ab94ce321f305b208ddd67e49b0
Luke Baertlein <luke.baertlein.oldsmumn@gmail.com>
2025-02-07 16:22:31 -0400

    continue on initial build

--- code/funcs.R
+++ code/funcs.R
@@ -1,5 +1,8 @@
+# Function to add suffix to dates
 add_ordinal_suffix <- function(day) {
   suffixes <- c("th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th")
   if (day %% 100 %in% c(11, 12, 13)) return(paste0(day, "th"))
   return(paste0(day, suffixes[day %% 10 + 1]))
 }
+
+
